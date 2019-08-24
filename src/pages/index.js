d3116f17a89bf6416e381fddfb418c2a6bf5cfaf
ApiratChiwakanit <apirat.chiwa@gmail.com>
2019-08-24 15:01:55 +0700

    update home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 export default function() {
     return <div>
-        <h1 className="title">Home</h1>  
+        <h1 className="title">Home</h1>
+        <p>Welcome !</p>
     </div>
 }
